a28f3ba17256d8417733a2afb9498b14dea33ebb
s1zzer <chepko_daniil@mail.ru>
2023-04-22 09:14:37 +0300

    Fix

--- Решение/Color_итог.py
+++ Решение/Color_итог.py
@@ -7,31 +7,31 @@ def gcd_list(numbers):
 
 colors = []
 while True:
-    color = input("Tsvet or enter: ")
+    color = input("Введите цвет или нажмите Enter: ")
     if color == "":
         break
     color = color.split(",")
     color = [int(x) for x in color]
     if any(c < 0 or c > 255 for c in color):
-        print("no")
+        print("Ошибка: значение цвета должно быть от 0 до 255.")
     else:
         colors.append(color)
 
-ratio = input("Final tsvet: ").split(",")
+ratio = input("Введите желаемый цвет в формате RGB (например, 128,128,128): ").split(",")
 ratio = [int(x) for x in ratio]
 
 if any(c < 0 or c > 255 for c in ratio):
-    print("no")
+    print("Ошибка: значение цвета должно быть от 0 до 255.")
 elif len(colors) < 2:
-    print("no")
+    print("Ошибка: для расчета коэффициентов смешивания необходимо как минимум два цвета.")
 else:
     min_coeff_sum = float('inf')
     best_coefficients = None
     max_search_range = 30
 
     for coefficients in itertools.product(range(1, max_search_range), repeat=len(colors)):
-        if sum(coefficients) == 0:
-            continue
+        # if sum(coefficients) == 0:
+        #     continue
         mixed_color = [sum(colors[j][i] * coefficients[j] for j in range(len(colors))) // sum(coefficients) for i in range(3)]
         if mixed_color == ratio:
             coeff_sum = sum(coefficients)
@@ -44,4 +44,4 @@ else:
         simplified_coefficients = [c // gcd for c in best_coefficients]
         print("Коэффициенты для смешивания цветов:", " : ".join(map(str, simplified_coefficients)))
     else:
-        print("Не удалось найти коэффициенты для смешивания цветов")
+        print("Не удалось найти коэффициенты для смешивания цветов.")
--- Решение/Итог_2_окр.py
+++ Решение/Итог_2_окр.py
@@ -2,95 +2,84 @@ import cv2
 import numpy as np
 from sklearn.cluster import DBSCAN
 
+def process_image(image_path, circle_radius=5, circle_margin=2):
+    def generate_circle_grid(image, radius, margin=0):
+        circles = []
+        y_step = int(radius * np.sqrt(3))
+        x_step = 2 * radius + margin
 
-def generate_circle_grid(image, radius, margin=0):
-    circles = []
-    y_step = int(radius * np.sqrt(3))
-    x_step = 2 * radius + margin
+        for y in range(radius, image.shape[0], y_step):
+            offset = 0 if y % (2 * y_step) == 0 else radius
+            for x in range(offset + radius, image.shape[1], x_step):
+                circles.append((y, x))
 
-    for y in range(radius, image.shape[0], y_step):
-        offset = 0 if y % (2 * y_step) == 0 else radius
-        for x in range(offset + radius, image.shape[1], x_step):
-            circles.append((y, x))
+        return circles
 
-    return circles
+    image = cv2.imread(image_path)
 
+    color_coords = np.argwhere(np.any(image != [255, 255, 255], axis=-1))
 
-# Загрузить изображение
+    dbscan = DBSCAN(eps=5, min_samples=20)
+    dbscan.fit(color_coords)
+
+    clusters = dbscan.labels_
+
+    circles_image = image.copy()
+
+    circle_count = 0
+    circle_data = []
+    for y, x in generate_circle_grid(circles_image, circle_radius, circle_margin):
+        indices = np.where((color_coords == [y, x]).all(axis=1))[0]
+        if indices.size > 0 and clusters[indices[0]] != -1:
+            cv2.circle(circles_image, (x, y), circle_radius, (0, 0, 0), 1)
+
+            mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
+            cv2.circle(mask, (x, y), circle_radius, (255, 255, 255), -1)
+
+            pixel_colors = image[np.where(mask == 255)]
+
+            circle_color = np.mean(pixel_colors, axis=0)
+
+            print(f"Circle {circle_count + 1} ({x}, {y}): {circle_color}")
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.5
+            font_thickness = 1
+            text = str(circle_count + 1)
+            text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+            text_x = x - text_size[0] // 2
+            text_y = y + text_size[1] // 2
+            cv2.putText(circles_image, text, (text_x, text_y), font, font_scale, (0, 0, 0), font_thickness)
+
+            circle_data.append({"id": circle_count + 1, "coordinates": (x, y), "color": circle_color})
+
+            circle_count += 1
+
+    gray_image = cv2.cvtColor(circles_image, cv2.COLOR_BGR2GRAY)
+
+    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
+    edges = cv2.Canny(blurred_image, 100, 200)
+
+    bordered_image = circles_image.copy()
+
+    border_color = (0, 0, 0)
+    border_thickness = 1
+    for y in range(edges.shape[0]):
+        for x in range(edges.shape[1]):
+            if edges[y, x] != 0:
+                cv2.rectangle(bordered_image, (x, y), (x, y), border_color, border_thickness)
+
+    cv2.imshow('Bordered Image', bordered_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return circle_data
+
+    # Пример использования функции
+
+"""
 image_path = 'image.png'
-image = cv2.imread(image_path)
-
-# Найти координаты пикселей с цветом
-color_coords = np.argwhere(np.any(image != [255, 255, 255], axis=-1))
-
-# Применить алгоритм DBSCAN к координатам пикселей объекта
-dbscan = DBSCAN(eps=5, min_samples=20)
-dbscan.fit(color_coords)
-
-# Получить кластеры
-clusters = dbscan.labels_
-
-# Выбрать радиус и зазор для окружностей
-circle_radius = 15
-circle_margin = 2
-
-# Создать изображение с нарисованными окружностями
-circles_image = image.copy()
-
-# Рисовать окружности на изображении и подписать их номера
-circle_count = 0
-for y, x in generate_circle_grid(circles_image, circle_radius, circle_margin):
-    indices = np.where((color_coords == [y, x]).all(axis=1))[0]
-    if indices.size > 0 and clusters[indices[0]] != -1:
-        cv2.circle(circles_image, (x, y), circle_radius, (0, 0, 0), 1)
-
-        # Создать маску для определения пикселей, попавших внутрь окружности
-        mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
-        cv2.circle(mask, (x, y), circle_radius, (255, 255, 255), -1)
-
-        # Вычислить цвет пикселей внутри окружности
-        pixel_colors = image[np.where(mask == 255)]
-
-        # Средний цвет пикселей внутри окружности
-        circle_color = np.mean(pixel_colors, axis=0)
-
-        # Вывести информацию о номере окружности и цвете
-        print(f"Circle {circle_count + 1} ({x}, {y}): {circle_color}")
-
-        # Добавить номер окружности на изображение
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        font_scale = 0.5
-        font_thickness = 1
-        text = str(circle_count + 1)
-        text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
-        text_x = x - text_size[0] // 2
-        text_y = y + text_size[1] // 2
-        cv2.putText(circles_image, text, (text_x, text_y), font, font_scale, (0, 0, 0), font_thickness)
-
-        circle_count += 1
-
-# Преобразовать изображение в оттенки серого
-gray_image = cv2.cvtColor(circles_image, cv2.COLOR_BGR2GRAY)
-
-# Применить размытие Гаусса для уменьшения шума
-blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-
-# Применить алгоритм Canny для обнаружения границ
-edges = cv2.Canny(blurred_image, 100, 200)
-
-# Создать копию исходного изображения
-bordered_image = circles_image.copy()
-
-# Нарисовать границы на изображении
-border_color = (0, 0, 0)  # Черный цвет
-border_thickness = 1
-for y in range(edges.shape[0]):
-    for x in range(edges.shape[1]):
-        if edges[y, x] != 0:
-            cv2.rectangle(bordered_image, (x, y), (x, y), border_color, border_thickness)
-
-# Вывести изображение с нарисованными окружностями, номерами и границами
-cv2.imshow('Bordered Image', bordered_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+circle_data = process_image(image_path)
+print(circle_data)
+"""
